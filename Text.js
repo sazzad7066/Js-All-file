d8d5fad97c4ec0cc653f26b517611ede22139091
sazzad7066 <sazzadulislam7066@gmail.com>
2025-11-12 11:47:44 +0600

    js file

---
+++ Text.js
@@ -0,0 +1,17 @@
+// synchronous programing
+
+const taskOne = () => {
+    console.log("task1");
+};
+const taskTwo = () => {
+    console.log("task2");
+};
+const taskThree = () => {
+    console.log("task3");
+};
+const taskFour = () => {
+    console.log("task4");
+};
+const taskFive = () => {
+    console.log("task5");
+};
